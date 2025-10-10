8e2a23648931e98b979428b200499647219ef11f
Warlock-Greg <98331029+Warlock-Greg@users.noreply.github.com>
2025-10-10 19:42:30 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,19 @@
+export const SHEET_JSON_URL = "https://opensheet.elk.sh/1bv_ABhNRuJAfsAgzlobHLkvTsEyYWOTLeRynhhouGGI/Feuille1";
+export const EMAILJS_SERVICE_ID = "service_03in3vd";
+export const EMAILJS_TEMPLATE_ID = "template_fxjeymy";
+export const EMAILJS_PUBLIC_KEY = "fKAFOoAJpLiNTwDHe";
 
+export const OWNER = "Warlock-Greg";
+export const REPO  = "parfect-golf";
+export const BRANCH = "main";
+export const GH_RAW_BASE = `https://raw.githubusercontent.com/${OWNER}/${REPO}/${BRANCH}/data`;
+
+export const EXERCISE_JSON_URL = `${GH_RAW_BASE}/exercises.json`;
+export const GOLFS_JSON_URL    = `${GH_RAW_BASE}/golfs.json`;
+
+export const LS_KEYS = {
+  LICENSE: 'parfect_license',
+  HISTORY: 'parfectHistory',
+  TRAIN:   'parfectTraining',
+  COACH_TONE: 'coachTone'
+};
