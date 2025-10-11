1baf16e63bb6ae8eeb0eca1e91e55baff5003ff1
Warlock-Greg <98331029+Warlock-Greg@users.noreply.github.com>
2025-10-11 09:39:51 +0200

    V6 clean

--- js/data.js
+++ js/data.js
@@ -1,11 +1,11 @@
-import { EXERCISE_JSON_URL, GOLFS_JSON_URL } from './config.js';
+import { GOLFS_JSON_URL, EXERCISE_JSON_URL } from "./config.js";
 
 export async function fetchGolfs() {
-  const res = await fetch(GOLFS_JSON_URL, { cache: "no-store" });
+  const res = await fetch(GOLFS_JSON_URL);
   return res.ok ? res.json() : [];
 }
 
 export async function fetchExercises() {
-  const res = await fetch(EXERCISE_JSON_URL, { cache: "no-store" });
+  const res = await fetch(EXERCISE_JSON_URL);
   return res.ok ? res.json() : [];
 }
